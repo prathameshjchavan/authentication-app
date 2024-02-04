891a51036c9f13e6327a25c97c19ef81edd296f4
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-04 20:44:41 +0530

    add google and github oauth providers

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
 import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import type { NextAuthConfig } from "next-auth";
 
 import { LoginSchema } from "./schemas";
@@ -7,6 +9,14 @@ import { getUserByEmail } from "./data/user";
 
 export default {
 	providers: [
+		Google({
+			clientId: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET
+		  }),
+		GitHub({
+			clientId: process.env.GITHUB_CLIENT_ID,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET,
+		}),
 		Credentials({
 			async authorize(credentials) {
 				const validatedFields = LoginSchema.safeParse(credentials);
--- auth.ts
+++ auth.ts
@@ -42,6 +42,18 @@ export const {
 			return token;
 		},
 	},
+	events: {
+		linkAccount: async ({ user }) => {
+			await db.user.update({
+				where: { id: user.id },
+				data: { emailVerified: new Date() },
+			});
+		},
+	},
+	pages: {
+		signIn: "/auth/login",
+		error: "/auth/error",
+	},
 	adapter: PrismaAdapter(db),
 	session: { strategy: "jwt" },
 	...authConfig,
