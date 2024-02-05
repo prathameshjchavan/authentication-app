27ef21f6ca1a59ef9f3271144b2024e3f942d6c5
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-05 17:51:23 +0530

    create generateVerificationToken function

---
+++ lib/tokens.ts
@@ -0,0 +1,28 @@
+import { v4 as uuidv4 } from "uuid";
+
+import { getVerificationTokenByEmail } from "@/data/getVerificationToken";
+import { db } from "./db";
+
+export const generateVerificationToken = async (email: string) => {
+	const token = uuidv4();
+	// expire the token in 1 hour
+	const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+	const existingToken = await getVerificationTokenByEmail(email);
+
+	if (existingToken) {
+		await db.verificationToken.delete({
+			where: { id: existingToken.id },
+		});
+	}
+
+	const verificationToken = await db.verificationToken.create({
+		data: {
+			email,
+			token,
+			expires,
+		},
+	});
+
+	return verificationToken;
+};
