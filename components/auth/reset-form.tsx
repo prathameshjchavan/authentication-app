74eb05bce5ee3d6da1c645e6468cf5593ce5d146
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-07 18:35:53 +0530

    add reset password page

---
+++ components/auth/reset-form.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useState, useTransition } from "react";
+
+import CardWrapper from "./card-wrapper";
+import { ResetSchema } from "@/schemas";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import FormError from "../form-error";
+import FormSuccess from "../form-success";
+
+const ResetForm = () => {
+	const [isPending, startTransition] = useTransition();
+	const [error, setError] = useState<string | undefined>("");
+	const [success, setSuccess] = useState<string | undefined>("");
+
+	const form = useForm<z.infer<typeof ResetSchema>>({
+		resolver: zodResolver(ResetSchema),
+		defaultValues: {
+			email: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof ResetSchema>) => {
+		setError("");
+		setSuccess("");
+
+		startTransition(() => {
+			console.log(values)
+		});
+	};
+
+	return (
+		<CardWrapper
+			headerLabel="Forgot your password?"
+			backButtonLabel="Back to login"
+			backButtonHref="/auth/login"
+		>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+					<FormField
+						control={form.control}
+						name="email"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Email</FormLabel>
+								<FormControl>
+									<Input
+										disabled={isPending}
+										placeholder="john.doe@example.com"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+					<FormError message={error} />
+					<FormSuccess message={success} />
+					<Button disabled={isPending} type="submit" className="w-full">
+						Send reset email
+					</Button>
+				</form>
+			</Form>
+		</CardWrapper>
+	);
+};
+
+export default ResetForm;
