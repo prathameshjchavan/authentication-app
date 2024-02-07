88342cf04c6290bfb4b689b620d649325d8a4a25
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-07 19:33:20 +0530

    create generate-password-reset-token function

--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,8 @@
 import { v4 as uuidv4 } from "uuid";
 
-import { getVerificationTokenByEmail } from "@/data/get-verification-token";
+import { getVerificationTokenByEmail } from "@/data/verification-token";
 import { db } from "./db";
+import { getPasswordResetTokenByEmail } from "@/data/password-reset-token";
 
 export const generateVerificationToken = async (email: string) => {
 	const token = uuidv4();
@@ -25,4 +26,28 @@ export const generateVerificationToken = async (email: string) => {
 	});
 
 	return verificationToken;
+};
+
+export const generatePasswordResetToken = async (email: string) => {
+	const token = uuidv4();
+	// expire the token in 1 hour
+	const expires = new Date(new Date().getTime() + 3600 * 1000);
+
+	const existingToken = await getPasswordResetTokenByEmail(email);
+
+	if (existingToken) {
+		await db.passwordResetToken.delete({
+			where: { id: existingToken.id },
+		});
+	}
+
+	const passwordResetToken = await db.passwordResetToken.create({
+		data: {
+			email,
+			token,
+			expires,
+		},
+	});
+
+	return passwordResetToken;
 };
