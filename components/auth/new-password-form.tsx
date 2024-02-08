a830552d2b3f77e4f8fe922316a1f5a631753f16
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-08 13:54:54 +0530

    create new password form

---
+++ components/auth/new-password-form.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useState, useTransition } from "react";
+import { useSearchParams } from "next/navigation";
+
+import CardWrapper from "./card-wrapper";
+import { NewPasswordSchema } from "@/schemas";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import FormError from "../form-error";
+import FormSuccess from "../form-success";
+
+const NewPasswordForm = () => {
+	const [isPending, startTransition] = useTransition();
+	const [error, setError] = useState<string | undefined>("");
+	const [success, setSuccess] = useState<string | undefined>("");
+	const searchParams = useSearchParams();
+	const token = searchParams.get("token");
+
+	const form = useForm<z.infer<typeof NewPasswordSchema>>({
+		resolver: zodResolver(NewPasswordSchema),
+		defaultValues: {
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof NewPasswordSchema>) => {
+		setError("");
+		setSuccess("");
+
+		console.log({ values });
+
+		// startTransition(() => {
+		// 	reset(values).then((data) => {
+		// 		setError(data.error);
+		// 		setSuccess(data.success);
+		// 	});
+		// });
+	};
+
+	return (
+		<CardWrapper
+			headerLabel="Enter a new password"
+			backButtonLabel="Back to login"
+			backButtonHref="/auth/login"
+		>
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+					<FormField
+						control={form.control}
+						name="password"
+						render={({ field }) => (
+							<FormItem>
+								<FormLabel>Password</FormLabel>
+								<FormControl>
+									<Input
+										disabled={isPending}
+										type="password"
+										placeholder="******"
+										{...field}
+									/>
+								</FormControl>
+								<FormMessage />
+							</FormItem>
+						)}
+					/>
+
+					<FormError message={error} />
+					<FormSuccess message={success} />
+					<Button disabled={isPending} type="submit" className="w-full">
+						Reset password
+					</Button>
+				</form>
+			</Form>
+		</CardWrapper>
+	);
+};
+
+export default NewPasswordForm;
