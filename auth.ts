ee88d84a5baba742d718cdfd5257809bccd60f32
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-09 11:56:00 +0530

    avoid sign-in if no 2fa confirmation

--- auth.ts
+++ auth.ts
@@ -1,3 +1,4 @@
+import { getTwoFactorConfirmationByUserId } from "./data/two-factor-confirmation";
 import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { UserRole } from "@prisma/client";
@@ -28,7 +29,20 @@ export const {
 			// Prevent sign in without email verification
 			if (!existingUser?.emailVerified) return false;
 
-			// TODO: Add 2FA check
+			if (existingUser.isTwoFactorEnabled) {
+				const twoFactorConfirmation = await getTwoFactorConfirmationByUserId(
+					existingUser.id
+				);
+
+				if (!twoFactorConfirmation) return false;
+
+				// Delete two factor confirmation for next sign in
+				await db.twoFactorConfirmation.delete({
+					where: { id: twoFactorConfirmation.id },
+				});
+
+				return true;
+			}
 
 			return true;
 		},
