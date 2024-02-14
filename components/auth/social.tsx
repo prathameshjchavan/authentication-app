82930b8679ae9c28dc40229077ce1be09eeb5712
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2024-02-14 19:19:24 +0530

    add redirect url for social logins

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -6,11 +6,15 @@ import { signIn } from "next-auth/react";
 
 import { Button } from "../ui/button";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { useSearchParams } from "next/navigation";
 
 const Social = () => {
+	const searchParams = useSearchParams();
+	const callbackUrl = searchParams.get("callbackUrl");
+
 	const onClick = (provider: "google" | "github") => {
-		signIn(provider, {
-			callbackUrl: DEFAULT_LOGIN_REDIRECT,
+		return signIn(provider, {
+			callbackUrl: callbackUrl || DEFAULT_LOGIN_REDIRECT,
 		});
 	};
 
